58ca6e07c8a6f3915118c315e7ffce936506c3b9
SEAN_RABBITWERKS <rabbitwerks.js@gmail.com>
2020-01-19 10:59:19 -0600

    add:  now-palying cmp

--- src/views/Twitch_Display.vue
+++ src/views/Twitch_Display.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="display">
+    <Now-Playing--Display />
   </div>
 </template>
+
+<script>
+import Current_Track from '../components/now-playing/Now_Playing.vue';
+
+export default {
+  components: {
+    'Now-Playing--Display': Current_Track,
+  },
+};
+</script>
